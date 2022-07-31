3ee8a6975ccb9f98045ebf16064f3a6569120d4d
bogdan demchenko <bogdandemchenko@gmail.com>
2022-07-31 15:00:29 +0300

    autoload fix

--- components/Autoload.php
+++ components/Autoload.php
@@ -1,16 +1,30 @@
 <?php
 
-function __autoload ($class_name)
-{
-	$array_paths = array(
-		'/models/',
-		'/components/'
-	);
+//function __autoload ($class_name)
+//{
+//	$array_paths = array(
+//		'/models/',
+//		'/components/'
+//	);
+//
+//	foreach ($array_paths as $path) {
+//		$path = ROOT . $path . $class_name . '.php';
+//		if (is_file($path)) {
+//			include_once $path;
+//		}
+//	}
+//}
 
-	foreach ($array_paths as $path) {
-		$path = ROOT . $path . $class_name . '.php';
-		if (is_file($path)) {
-			include_once $path;
-		}
-	}
-}
+spl_autoload_register(function ($class_name) {
+    $array_paths = array(
+        '/models/',
+        '/components/'
+    );
+
+    foreach ($array_paths as $path) {
+        $path = ROOT . $path . $class_name . '.php';
+        if (is_file($path)) {
+            include_once $path;
+        }
+    }
+});
